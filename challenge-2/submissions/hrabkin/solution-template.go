207788940dd61bac443be15ff9567607336fd9d5
Henadzi Rabkin <g.rjabkin@gmail.com>
2026-01-24 16:06:55 +0100

    Add solution for Challenge 2 by hrabkin

---
+++ challenge-2/submissions/hrabkin/solution-template.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	// Read input from standard input
+	scanner := bufio.NewScanner(os.Stdin)
+	if scanner.Scan() {
+		input := scanner.Text()
+
+		// Call the ReverseString function
+		output := ReverseString(input)
+
+		// Print the result
+		fmt.Println(output)
+	}
+}
+
+// ReverseString returns the reversed string of s.
+func ReverseString(s string) string {
+	// TODO: Implement the function
+	var i,j = 0, len(s)-1
+	runes := make([]rune,len(s))
+	for idx,r := range s {
+	    runes[idx] = r
+	}
+	for i < j {
+	    runes[i],runes[j] = runes[j],runes[i]
+	    i++
+	    j--
+	}
+    	
+	return string(runes)
+}
